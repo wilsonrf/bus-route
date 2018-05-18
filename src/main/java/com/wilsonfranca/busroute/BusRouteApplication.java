0be84709dade5f20021a88136666af411a46d230
Wilson da Rocha França <wilsonrf@gmail.com>
2018-05-17 21:34:51 -0300

    First commit

---
+++ src/main/java/com/wilsonfranca/busroute/BusRouteApplication.java
@@ -0,0 +1,12 @@
+package com.wilsonfranca.busroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusRouteApplication.class, args);
+	}
+}
